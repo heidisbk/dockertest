99bdb990f1c2bfc1bf91ca93c3516f8b8905f3b5
Heidi <heidi.souibki@gmail.com>
2022-07-13 16:37:48 +0200

    continuous integration

--- test_data_preparation.py
+++ test_data_preparation.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def test_prepare_and_normalise_dataset():
     # Test sans intéret mais seulement pour montrer qu'on utilise 'pytest' au déploiement.
-    expected = [10000, 11]
-    df = pd.read_csv('./Churn_Modelling.csv', sep=',', header=0)
+    expected = [40, 5]
+    df = pd.read_csv('./houses.csv', sep=',', header=0)
     results = [len(df.axes[0]), len(df.axes[1])]
     assert expected == results
